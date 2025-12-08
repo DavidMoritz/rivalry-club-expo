6103e76e13643a98fa11025d6ce84f5eaa9cb73c
David Moritz <david@jde-systems.com>
2025-12-08 10:03:15 -0600

    WIP: still broken build

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const isTest = process.env.NODE_ENV === 'test';
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: isTest
-      ? [
-          // Disable worklets plugin in test environment
-        ]
-      : [],
-    env: {
-      test: {
-        plugins: [
-          // Override to disable worklets in tests
-          ['react-native-worklets/plugin', { enabled: false }],
-        ],
-      },
-    },
   };
 };
